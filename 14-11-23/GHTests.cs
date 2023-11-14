ba4de10735d71ee0f3f58a3fba839532e77d95dd
ParvathyPanayingal <148305026+ParvathyPanayingal@users.noreply.github.com>
2023-11-14 18:18:10 +0530

    Add files via upload

---
+++ 14-11-23/GHTests.cs
@@ -0,0 +1,110 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium.Edge;
+
+namespace SelfExamples
+{
+    internal class GHTests
+    {
+        IWebDriver? driver;
+        
+
+        public void InitializeChromeDriver()
+        {
+            driver = new ChromeDriver(); //browser will open automatically
+            driver.Url = "https://www.google.com/";
+            driver.Manage().Window.Maximize();
+        }
+
+        public void InitializeEdgeDriver()
+        {
+            driver = new EdgeDriver(); //browser will open automatically
+            driver.Url = "https://www.google.com/";
+            driver.Manage().Window.Maximize();
+
+        }
+
+        public void TitleTest()
+        {
+            Thread.Sleep(2000); // To wait (10 seconds), only for viewing purpose .Should not include in the final code.
+            Console.WriteLine("title" + driver.Title);
+            Console.WriteLine("Titile length " + driver.Title.Length);
+            Assert.AreEqual("Google", driver.Title);
+            Console.WriteLine("Title test - Pass");
+        }
+
+        public void PageSourceandURLTest()
+        {
+            //Console.WriteLine("PS Len : " + driver.PageSource.Length);
+            //Console.WriteLine(driver.Url);
+            Assert.AreEqual("https://www.google.com/", driver.Url);
+            Console.WriteLine("Page source and url test - Pass");
+
+        }
+
+        public void GoogleSearchTest()
+        {
+            IWebElement searchInputBox = driver.FindElement(By.Id("APjFqb"));
+            searchInputBox.SendKeys("hp laptops"); //to type inside the text box
+            Thread.Sleep(5000);
+            //IWebElement googleSearchButton = driver.FindElement(By.Name("btnK"));
+            IWebElement googleSearchButton = driver.FindElement(By.ClassName("gNO89b"));
+
+            googleSearchButton.Click();
+            Assert.AreEqual("hp laptops - Google Search", driver.Title);
+            Console.WriteLine("GoogleSearchTest - Pass");
+        }
+
+        public void GmailLinkTest()
+        {
+            // driver.FindElement(By.LinkText("Gmail")).Click(); //not need of iwebdriver since we are not using it again.
+            driver.Navigate().Back();
+            driver.FindElement(By.LinkText("Gmail")).Click();
+            Thread.Sleep(2000);
+            Assert.That(driver.Url.Contains("gmail")); //if we don't know what we will get then use 'that'.
+            Console.WriteLine("Gmail link -Pass");
+;        }
+
+        public void ImagesLinkTest()
+        {
+            driver.Navigate().Back();
+            driver.FindElement(By.PartialLinkText("mag")).Click();
+            Thread.Sleep(2000);
+            Assert.That(driver.Title.Contains("Images")); //if we don't know what we will get then use 'that'.
+            Console.WriteLine("Image link -Pass");
+            ;
+        }
+
+        public void LocalizationTest()
+        {
+            driver.Navigate().Back();
+            string loc = driver.FindElement(By.XPath("/html/body/div[1]/div[6]/div[1]")).Text;
+            Thread.Sleep(1000);
+            Assert.That(loc.Equals("India")); 
+            Console.WriteLine("Localization -Pass");
+            ;
+        }
+
+        public void GAppYoutube()
+        {
+            driver.FindElement(By.ClassName("gb_d")).Click();
+            Thread.Sleep(3000);
+            driver.FindElement(By.ClassName(""));
+            Thread.Sleep(3000);
+            Assert.That("Youtube".Equals(driver.Title));
+
+        }
+
+        public void Destruct()
+        {
+            driver.Close();
+
+        }
+    }
+}
---
+++ 14-11-23/Program.cs
@@ -0,0 +1,52 @@
+﻿
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SelfExamples;
+
+GHTests gHTests = new ();
+
+/*
+Console.WriteLine("1.Edge 2.Chrome");
+int ch=Convert.ToInt32(Console.ReadLine());
+switch (ch)
+{
+    case 1:
+        gHTests.InitializeEdgeDriver();break;
+    case 2:
+        gHTests.InitializeChromeDriver();break;
+}
+*/
+List<string> drivers = new List<string>();
+drivers.Add("Chrome");
+drivers.Add("Edge");
+
+foreach (var d in drivers)
+{
+    switch (d)
+    {
+        case "Chrome":
+            gHTests.InitializeChromeDriver(); break;
+        case "Edge":
+            gHTests.InitializeEdgeDriver(); break;
+
+    }
+    try
+    {
+        gHTests.TitleTest();
+        gHTests.PageSourceandURLTest();
+        gHTests.GoogleSearchTest();
+        gHTests.GmailLinkTest();
+        gHTests.ImagesLinkTest();
+        gHTests.LocalizationTest();
+        //gHTests.GAppYoutube();
+
+    }
+    catch (AssertionException)
+    {
+        Console.WriteLine("Fail");
+    }
+    gHTests.Destruct();
+}
+
+
