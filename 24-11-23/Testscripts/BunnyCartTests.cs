006ecfe9f22fd3942565f760c31cc86f9392043c
ParvathyPanayingal <148305026+ParvathyPanayingal@users.noreply.github.com>
2023-11-24 17:42:21 +0530

    Add files via upload

---
+++ 24-11-23/Testscripts/BunnyCartTests.cs
@@ -0,0 +1,51 @@
+﻿using BunnyCart.PageObjects;
+using BunnyCart.Utilities;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BunnyCart.Testscripts
+{
+    [TestFixture]
+    internal class BunnyCartTests:CoreCodes
+    {
+        [Test]
+        public void SignUpTest()
+        {
+            BunnyCartHomePage bchp = new(driver);
+
+            bchp.CreateAccLinkClick();
+            Thread.Sleep(2000);
+
+            string currentDirectory = Directory.GetParent(@"../../../").FullName; //taking dirctory of config file from working directory.
+            string excelFilePath = currentDirectory + "/TestData/InputData.xlsx";
+            string? sheetname = "CreateAccount";
+
+            List<SignUp> excelDataList = ExcelUtils.ReadSignUpExcelData(excelFilePath,sheetname);
+
+
+
+
+
+
+
+
+            try
+            {
+                Assert.That(driver.FindElement(By.XPath("//div[" +
+                    "@class='modal-inner-wrap']//following::h1[2]")).Text, Is.EqualTo("Create an Account"));
+
+                bchp.SignUp("john", "Doe", "Johndoe@xample.com", "12345", "12345", "9009009009");
+            }
+            catch
+            {
+                Console.WriteLine("Create acc modal not present");
+            }
+        }
+
+        
+    }
+}
---
+++ 24-11-23/Testscripts/SearchTests.cs
@@ -0,0 +1,71 @@
+﻿using BunnyCart.PageObjects;
+using BunnyCart.Utilities;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BunnyCart.Testscripts
+{
+    [TestFixture]
+    internal class SearchTests:CoreCodes
+    {
+        [Test]
+        public void CreateAccountTest()
+        {
+            BunnyCartHomePage bchp = new(driver);
+
+            bchp.CreateAccLinkClick();
+            Thread.Sleep(2000);
+
+            Assert.That(driver?.FindElement(By.XPath("//div[" +
+                "@class='modal-inner-wrap']//following::h1[2]")).Text,
+                Is.EqualTo("Create an Account"));
+
+            try
+            {
+                Assert.That(driver?.FindElement(By.XPath("//div[" +
+                    "@class='modal-inner-wrap']//following::h1[2]")).Text,
+                    Is.EqualTo("Create an Account"));
+                test = extent.CreateTest("Create Account Link Test - Pass");
+                test.Pass("Create Account Link success");
+                Console.WriteLine("ERCP");
+            }
+            catch
+            {
+                test = extent.CreateTest("Create Account Link Test - Fail");
+                test.Fail("Create Account Link failed");
+                Console.WriteLine("ERCF");
+            }
+
+
+            string? currDir = Directory.GetParent(@"../../../")?.FullName;
+            string? excelFilePath = currDir + "/TestData/InputData.xlsx";
+            string? sheetName = "CreateAccount";
+
+            List<SignUp> excelDataList = ExcelUtils.ReadSignUpExcelData(excelFilePath, sheetName);
+
+            foreach (var excelData in excelDataList)
+            {
+
+                string? firstName = excelData?.FirstName;
+                string? lastName = excelData?.LastName;
+                string? email = excelData?.Email;
+                string? pwd = excelData?.Password;
+                string? conpwd = excelData?.ConfirmPassword;
+                string? mbno = excelData?.MobileNumber;
+
+                Console.WriteLine($"First Name: {firstName}, Last Name: {lastName}, Email: {email}, Password: {pwd}, Confirm Password: {conpwd}, Mobile Number: {mbno}");
+
+
+                bchp.SignUp(firstName, lastName, email, pwd, conpwd, mbno);
+
+            }
+
+        }
+
+    }
+}
+
